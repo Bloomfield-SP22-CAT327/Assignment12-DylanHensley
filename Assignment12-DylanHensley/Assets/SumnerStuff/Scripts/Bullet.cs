7af8a714e548a57442f8ddeeed08fabc348f38f2
Summy99 <dylan_hensley@bloomfield.edu>
2022-03-17 03:01:57 -0400

    Commit 8

--- Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Bullet.cs
+++ Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Bullet.cs
@@ -4,10 +4,18 @@ using Mirror;
 public class Bullet : NetworkBehaviour
 {
     [SyncVar]
+    public uint parentNetId;
+
+    [SyncVar]
     public Color color;
 
     public override void OnStartClient()
     {
         gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
--- Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Player.cs
+++ Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : NetworkBehaviour
     float moveSpeed = 1.875f;
 
     [SyncVar]
+    public int score;
+
+    [SyncVar]
     public Color color;
 
     void Update()
@@ -44,6 +47,7 @@ public class Player : NetworkBehaviour
         GameObject bullet = (GameObject)Instantiate(bulletPrefab, this.transform.position + this.transform.right, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * 17.5f;
         bullet.GetComponent<Bullet>().color = color;
+        bullet.GetComponent<Bullet>().parentNetId = this.netId;
         Destroy(bullet, 0.875f);
         NetworkServer.Spawn(bullet);
     }
