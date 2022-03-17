dce6637d66d6e951aadcf990f2c86c84ee4a2cde
Summy99 <dylan_hensley@bloomfield.edu>
2022-03-17 02:10:35 -0400

    Commit 4

--- Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Player.cs
+++ Assignment12-DylanHensley/Assets/SumnerStuff/Scripts/Player.cs
@@ -3,8 +3,27 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
+    float moveSpeed = 1.875f;
+
     [SyncVar]
     public Color color;
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        MoveIt(x, y);
+    }
+
+    void MoveIt(float x, float y)
+    {
+        transform.Translate(x, y, 0);
+    }
 
     public override void OnStartClient()
     {
